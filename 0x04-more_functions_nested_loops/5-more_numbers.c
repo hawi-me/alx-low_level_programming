3d98dc29d12d01cf47b66e2cd3bbdfd6c8c8d733
hawi-me <hawiabdi41.com>
2023-09-15 08:50:55 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *more_numbers: the function that prints 10 times number 0 to 14
+ *Return: 10 times the numbers
+ */
+
+void more_numbers(void)
+{
+	int i; 
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0 ; j <= 14; j++)
+		{
+			if (j > 0)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
