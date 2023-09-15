821cc7bd71f81d366ca317ae1f19df8f2dd221b8
hawi-me <hawiabdi41.com>
2023-09-15 05:48:01 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - function that print the multiple of two number
+ *@x: the first number
+ *@y: the second number
+ *Return: the multiple value
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
