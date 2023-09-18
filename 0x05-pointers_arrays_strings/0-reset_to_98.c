7fe4c0f5793a26a695a3f4e019407d375a0fe13d
hawi-me <hawiabdi41.com>
2023-09-18 06:22:49 -0700

    betty 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*reset_to_98 - function thakes pointer in to int
+*@n: variabel used to point
+*Retun: nothing
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+the read me file for the pointer array and strings project 
