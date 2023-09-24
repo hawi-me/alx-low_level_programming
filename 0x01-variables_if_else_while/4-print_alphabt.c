e2c499cd3676be30927d0d719b0e8d5dc7f55ce7
hawi-me <hawiabdi41.com>
2023-09-24 13:04:00 -0700

    printalphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - function  tbat print all alphabet  except q e
+ *Return: return 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
