21612853cee2fa639c4b70cc22ecb7bfa582c043
hawi-me <hawiabdi41.com>
2023-09-24 13:30:39 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - function that print numbers
+ *Return: zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
