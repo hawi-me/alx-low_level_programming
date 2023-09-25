39813a810e7764f16552f5a5d1e5c702f62cc508
hawi-me <hawiabdi41.com>
2023-09-25 03:09:13 -0700

    strchar

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strchr - funtion print chracter that have occurance
+ *@s: the first input charcheter
+ *@c: charchter to compare
+ *Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+		return (s + i);
+		}
+	}
+	return (NULL);
+}
