6fab1c2aaf507137126e7a2a80ae69b62b5cbfce
hawi-me <hawiabdi41.com>
2023-09-25 19:56:28 -0700

    strnp

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *_strspn - function taht print gets the length ofprefix string
+ *@s: the input string
+ *@accept: string
+ *Return: int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
